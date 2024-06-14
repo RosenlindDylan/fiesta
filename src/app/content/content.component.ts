6cfaba9b03e0d89fa57abf7dcb6e6c45970abaa0
RosenlindDylan <35314066+RosenlindDylan@users.noreply.github.com>
2024-06-14 13:00:00 -0700

    content component and requests

---
+++ src/app/content/content.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      content works!
+    </p>
+  `,
+  styleUrl: './content.component.css'
+})
+export class ContentComponent {
+  // get 
+}
